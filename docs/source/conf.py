6398abac8dbe5429b2f71a1de650bf31ce6290ca
mdeboute <martin.deboute@gmail.com>
2025-01-19 17:54:30 +0100

    wip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,23 +5,22 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import pystock
 
 project = "pystock"
 copyright = "2025, Martin Debouté"
 author = "Martin Debouté"
-release = "0.1.0"
+release = pystock.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "autodocsumm", "sphinx.ext.coverage"]
+auto_doc_default_options = {"autosummary": True}
 
-templates_path = ["_templates"]
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ["_static"]
---
+++ docs/source/pystock.rst
@@ -0,0 +1,45 @@
+pystock package
+===============
+
+Submodules
+----------
+
+pystock.asset module
+--------------------
+
+.. automodule:: pystock.asset
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pystock.constants module
+------------------------
+
+.. automodule:: pystock.constants
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pystock.portfolio module
+------------------------
+
+.. automodule:: pystock.portfolio
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pystock.quantitative module
+---------------------------
+
+.. automodule:: pystock.quantitative
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: pystock
+   :members:
+   :undoc-members:
+   :show-inheritance:
